28049f4b5512f78034ad39e9c3bbd14cbaaf2eb0
Tim Vink <vinktim@gmail.com>
2021-02-11 17:23:09 +0100

    python 3.6+ support

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ setup(
     version="0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=["Click", "python-dotenv", "sh>=1.14.1", "rich>=9.8.2"],
+    install_requires=["Click", "python-dotenv", "sh>=1.14.1", "rich>=9.10.0"],
     entry_points={
         "console_scripts": [
             "doing = doing.cli:cli",
         ]
     },
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
