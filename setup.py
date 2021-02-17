42fff95576700effc699f68559e40ea4bb562f9d
Tim Vink <vinktim@gmail.com>
2021-02-17 15:28:58 +0100

    Add README to long_description in setup.py closes #8

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+file = open("README.md", "r")
+LONG_DESCRIPTION = file.read()
+file.close()
+
 setup(
     name="doing-cli",
     version="0.1",
     packages=find_packages("src"),
     package_dir={"": "src"},
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
     install_requires=["Click", "python-dotenv", "rich>=9.10.0"],
     entry_points={
         "console_scripts": [
