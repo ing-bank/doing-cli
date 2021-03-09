eb0be9ee8d5ff1fcb4af4d6b5dcb6dae6f27659a
Tim Vink <vinktim@gmail.com>
2021-03-09 14:41:10 +0100

    Setup conditional dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ file = open("README.md", "r")
 LONG_DESCRIPTION = file.read()
 file.close()
 
+base_packages = ["Click", "python-dotenv", "rich>=9.10.0", "pyyaml>=5.4.0"]
+dev = ["mkdocs-material", "mkdocstrings", "pytest", "pytest-cov", "pyflakes"]
+
 setup(
     name="doing-cli",
     version="0.5.1",
@@ -11,7 +14,10 @@ setup(
     package_dir={"": "src"},
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    install_requires=["Click", "python-dotenv", "rich>=9.10.0", "pyyaml>=5.4.0"],
+    install_requires=base_packages,
+    extras_require={
+        "all": base_packages + dev,
+    },
     entry_points={
         "console_scripts": [
             "doing = doing.cli:cli",
