e1b5fbd462fa4ce4fa7145eb0104a290ad2164a4
Fitzgerald Krudde <fitzgerald.krudde@ing.com>
2021-06-18 21:21:38 +0200

    optionally assign story points to a new issue

--- src/doing/issue/create_issue.py
+++ src/doing/issue/create_issue.py
@@ -1,5 +1,5 @@
 from doing.exceptions import InputError
-from doing.utils import replace_user_aliases, run_command, get_az_devop_user_email
+from doing.utils import replace_user_aliases, run_command, get_az_devop_user_email, get_config
 
 from rich.console import Console
 
@@ -38,8 +38,13 @@ def cmd_create_issue(
         cmd += f"--assigned-to '{assignee}' "
     if body:
         cmd += f"--description '{body}' "
-    if label:
-        cmd += f"--fields 'System.Tags={label}' "
+    work_item_points = get_config("default_story_points", fallback="")
+    if(work_item_points):
+        cmd += f"--fields 'Microsoft.VSTS.Scheduling.StoryPoints={work_item_points}' "
+        if label:
+            cmd += f"' System.Tags={label}' "
+        elif label:
+            cmd += f"--fields ' System.Tags={label}' "
     cmd += f"--area '{area}' --iteration '{iteration}' --project '{project}' --organization '{organization}'"
 
     work_item = run_command(cmd)
