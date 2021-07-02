d2f70acf51dbf6cf6e15f9cbef2de33ce657f771
Fitzgerald Krudde <fitzgerald.krudde@ing.com>
2021-07-02 17:09:19 +0200

    removed leftover from merge main

--- src/doing/issue/create_issue.py
+++ src/doing/issue/create_issue.py
@@ -40,7 +40,6 @@ def cmd_create_issue(
         cmd += f"--assigned-to '{assignee}' "
     if body:
         cmd += f"--description '{body}' "
-    work_item_points = get_config("default_story_points", fallback="")
     if(work_item_points):
         cmd += f"--fields 'Microsoft.VSTS.Scheduling.StoryPoints={work_item_points}' "
         if label:
