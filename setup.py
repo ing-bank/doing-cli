352fe2cfad219974926d524148eaeae04c422379
Tim Vink <vinktim@gmail.com>
2021-12-24 14:46:17 +0100

    Drop explicit support for python 3.6 as it is end of life, add 3.9 and 3.10

--- setup.py
+++ setup.py
@@ -51,9 +51,10 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
